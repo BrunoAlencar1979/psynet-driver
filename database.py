8a8b72f3101530deb29523e73e1b7d3e8732d59d
Bruno de Alencar <84350650+BrunoAlencar1979@users.noreply.github.com>
2026-06-02 20:33:45 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # A URL virá de uma variável de ambiente (vamos configurar isso no Render)
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(postgresql://neondb_owner:npg_nTfWscgFY5x4@ep-blue-poetry-acwknpga-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require)
+engine = create_engine("postgresql://neondb_owner:npg_nTfWscgFY5x4@ep-blue-poetry-acwknpga-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
