f7eaa13c14fed02451b52e79f61bc67bb5006c33
Bruno de Alencar <84350650+BrunoAlencar1979@users.noreply.github.com>
2026-06-04 12:45:30 -0300

    Add user_id column to Lancamento model
    Added user_id column to Lancamento model with default value.

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from database import Base
-from datetime import datetime
+import datetime
 
 class Lancamento(Base):
     __tablename__ = "lancamentos"
 
     id = Column(Integer, primary_key=True, index=True)
-    tipo = Column(String(50), index=True)       # ganho ou despesa
+    user_id = Column(String(100), index=True, default="bruno_admin") # <--- A NOVA COLUNA AQUI
+    tipo = Column(String(50), index=True)
     valor = Column(Float)
     descricao = Column(String(255))
-    data_hora = Column(DateTime, default=datetime.utcnow)
+    data_hora = Column(DateTime, default=datetime.datetime.utcnow)
